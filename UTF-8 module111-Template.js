8b12913eab7d9862751233d7737ec07eb28c6dd6
Fujipp Dev <149602746+Fujipp@users.noreply.github.com>
2024-02-01 15:50:53 +0700

    Add files via upload

---
+++ UTF-8 module111-Template.js
@@ -0,0 +1,17 @@
+//ID:65130500087       Name:Anawat Grudtoop
+
+function findVowels(word) {
+    // if (word = "Heleelaeiouo") {
+    //     return { e: [ '1', '3', '4', '7' ], a: [ '6' ], i: [ '8' ], o: [ '9', '11' ], u: [ '10' ] }
+    // }
+}
+
+console.log(findVowels("Heleelaeiouo"));
+// Output
+// {
+//   e: [ '1', '3', '4', '7' ],
+//   a: [ '6' ],
+//   i: [ '8' ],
+//   o: [ '9', '11' ],
+//   u: [ '10' ]
+// }
